a9a564c719ddf28a1abcdb0219c16f5af3e33fd1
duglasvalentin <31623461+duglasvalentin@users.noreply.github.com>
2018-07-06 09:23:12 -0500

    Api services de GCP

---
+++ Traslate/src/java/Model/model.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author jordy
+ */
+public class model {
+    
+    private String token_acces="ya29.GlzwBWoTbP32_AKpC3KEbvcInVuILascsQGtRRGiVVQyLvaLCHzzfXQQRormlZDim0Y7DU7zjgO5yajK1byWjVNs1-24rj0DLoNWxrvb7mj7Oop8314lO7df5YAang";
+    private String responses;
+    private String nameFile;
+
+    public String getToken_acces() {
+        return token_acces;
+    }
+
+    public void setToken_acces(String token_acces) {
+        this.token_acces = token_acces;
+    }
+
+    public String getResponses() {
+        return responses;
+    }
+
+    public void setResponses(String responses) {
+        this.responses = responses;
+    }
+
+    public String getNameFile() {
+        return nameFile;
+    }
+
+    public void setNameFile(String nameFile) {
+        this.nameFile = nameFile;
+    }
+    
+    
+}
